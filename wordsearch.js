d1871413ad41580df4f61ea76c0662a7f91b2926
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-06 23:54:32 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word))
+            return true;
     }
-}
+    const verticalJoin = transposed(letters).map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    } return false;
+};
+const transposed = matrix => {
+    const newMatrix = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        newMatrix.push([]);
+        for (let j = 0; j < matrix.length; j++) {
+            newMatrix[i][j] = matrix[j][i];
+        }
+    }
+    return newMatrix;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch; 
